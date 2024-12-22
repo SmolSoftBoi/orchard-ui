9c7df3c96a164c701634a54223994597c70c7230
Kristian Matthews-Kennington <kristian@matthews-kennington.com>
2024-12-22 21:46:27 +0000

    add badges support and tap action to FloorHeadingCardStrategy for enhanced interactivity

--- src/strategies/floor-heading-card.ts
+++ src/strategies/floor-heading-card.ts
@@ -1,7 +1,11 @@
 import { Hass, HassFloorRegistryEntry } from '../hass';
-import { LovelaceCardConfig } from '../lovelace';
+import { LovelaceBadgeConfig, LovelaceCardConfig } from '../lovelace';
 
 export type FloorHeadingCardStretegyConfig = {
+  floor: HassFloorRegistryEntry;
+};
+
+export type FloorHeadingCardsStrategyBadgesConfig = {
   floor: HassFloorRegistryEntry;
 };
 
@@ -14,6 +18,18 @@ export class FloorHeadingCardStrategy {
       type: 'heading',
       heading: config.floor.name,
       icon: config.floor.icon || undefined,
+      tap_action: {
+        action: 'navigate',
+        navigation_path: `/${config.floor.floor_id}`,
+      },
+      badges: await this.generateBadges({ floor: config.floor }, hass),
     };
+  }
+
+  static async generateBadges(
+    config: FloorHeadingCardsStrategyBadgesConfig,
+    hass: Hass
+  ): Promise<LovelaceBadgeConfig[]> {
+    return [];
   }
 }
