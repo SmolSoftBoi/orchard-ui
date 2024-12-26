5ead066789619107177fbe0355d444dd46c7d6b6
Kristian Matthews-Kennington <kristian@matthews-kennington.com>
2024-12-26 18:12:18 +0000

    refactor: add Characteristic class and interface for improved service attribute handling

--- src/home/index.ts
+++ src/home/index.ts
@@ -3,6 +3,7 @@ import Zone from './zone';
 import Room from './room';
 import Accessory from './accessory';
 import Service from './service/service';
+import Characteristic from '../charactaristic';
 import User from './user';
 
-export { Home, Zone, Room, Accessory, Service, User };
+export { Home, Zone, Room, Accessory, Service, Characteristic, User };
